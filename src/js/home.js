c2ee29952fb762f80bd1a05c18f63ecc5d5853c6
ailenrmansilla <ailen.r.mansilla@gmail.com>
2024-10-04 15:08:00 -0400

    styles and projects page addded. New js functionalities

--- src/js/home.js
+++ src/js/home.js
@@ -1,6 +1,35 @@
 console.log("Helloo");
 
+
+// Reveal name in the title after a click on the button
 const button_name =  document.querySelector('#reveal_name');
 const my_name = document.getElementById('my_name');
 
-// addEventListener
+button_name.addEventListener("click", function(){
+    my_name.innerHTML = "Ailen"; 
+    my_name.style.color = "brown";
+});
+
+// Using document.lastModified property
+console.log(document.lastModified);
+let last_modified = document.lastModified;
+let date = new Date(last_modified);
+// Last modified year
+document.querySelector("#last_updated").innerHTML = date.getFullYear();
+
+// Including Contact me information in the footer
+
+let contact_me = document.querySelector('#contact_me');
+// content
+let email = 'man22007@byui.edu';
+let phone = '+17657301406';
+// create new nodes/elements
+const email_element = document.createElement('span');
+// insert new content or elements
+email_element.innerHTML = email;
+const phone_element = document.createElement('span');
+phone_element.innerHTML = phone;
+// append new elements
+contact_me.appendChild(email_element); // add new child at the end
+contact_me.appendChild(phone_element);
+
